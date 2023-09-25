9724b44489d8635fc46beb5ff69b9f7ae92de7ed
Richard Longdon <richardlongdon@richards-mbp.home>
2023-09-25 18:25:09 +0100

    hosts

--- golf_trip/settings.py
+++ golf_trip/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-=3^59i0#pi3h*zfl3d9(7!z^6nm5hk^g9q#zlux&!4w&(v=v!c
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['http://rgl1892.pythonanywhere.com']
 
 
 # Application definition
