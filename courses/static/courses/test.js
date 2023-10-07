fc7df339ad1301ac23928d3dc19b930da5e240a2
Richard Longdon <richardlongdon@Richards-MacBook-Pro.local>
2023-10-07 20:56:14 +0100

    bug on scores fixed

--- courses/static/courses/test.js
+++ courses/static/courses/test.js
@@ -1,4 +1,4 @@
-import * as d3 from "https://cdn.jsdelivr.net/npm/d3@7/+esm";
+// import * as d3 from "https://cdn.jsdelivr.net/npm/d3@7/+esm";
 var dataset = await d3.json('/api/holes');
 async function plot(val) {
     d3.selectAll('svg').remove()
