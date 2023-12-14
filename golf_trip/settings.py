9abcff7391f4c4c36dc80750da8e3a0c164c8dd9
rlongdon <rglongdon@gmail.com>
2023-12-14 21:34:07 +0000

    base dir

--- golf_trip/settings.py
+++ golf_trip/settings.py
@@ -125,8 +125,8 @@ USE_TZ = True
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 
-STATIC_ROOT = BASE_DIR / '/static/'
-MEDIA_ROOT = BASE_DIR / '/media/'
+STATIC_ROOT = BASE_DIR / 'static/'
+MEDIA_ROOT = BASE_DIR / 'media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
