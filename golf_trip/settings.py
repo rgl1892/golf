e004148829dfd62da4382e9bf758481dfc3bc874
rlongdon <rglongdon@gmail.com>
2023-12-14 21:35:46 +0000

    base dir

--- golf_trip/settings.py
+++ golf_trip/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-=3^59i0#pi3h*zfl3d9(7!z^6nm5hk^g9q#zlux&!4w&(v=v!c'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
                 '*',
